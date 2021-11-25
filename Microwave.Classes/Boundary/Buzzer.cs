ec7906b3abf7ef424ef4f0f34e9de973cb25baa1
au619496 <201804204@post.au.dk>
2021-11-25 09:42:51 +0100

    Created buzzer with test

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Microwave.Classes.Interfaces;
+
+namespace Microwave.Classes.Boundary
+{
+    public class Buzzer : IBuzzer
+    {
+        private readonly IOutput myOutput;
+        public void ShortBeep()
+        {
+            myOutput.OutputLine("Beep!");
+        }
+
+        public void ThreeShortBeeps()
+        {
+            for (int i = 0; i < 3; i++)
+                myOutput.OutputLine("Beep!");
+        }
+
+        public Buzzer(IOutput output)
+        {
+            myOutput = output;
+        }
+
+    }
+}
